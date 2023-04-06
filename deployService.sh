419c87022a33a4663a6c921a2458da099a3e2d82
Eric Upton-Rowley <rowleyup@gmail.com>
2023-04-05 18:26:33 -0600

    created database functions

--- deployService.sh
+++ deployService.sh
@@ -17,11 +17,28 @@ fi
 
 printf "\n----> Deploying files for $service to $hostname with $key.\n"
 
+printf "\n----> Build the distribution package\n"
+rm -rf dist
+mkdir dist
+cp -r public dist
+cp *.js dist
+cp package* dist
+
 printf "\n----> Clear out the previous distribution on the target.\n"
 ssh -i "$key" ubuntu@$hostname << ENDSSH
-rm -rf services/${service}/public
-mkdir -p services/${service}/public
+rm -rf services/${service}
+mkdir -p services/${service}
 ENDSSH
 
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i "$key" * ubuntu@$hostname:services/$service/public
+scp -r -i "$key" * ubuntu@$hostname:services/$service
+
+printf "\n----> Deploy the service on the target\n"
+ssh -i "$key" ubuntu@$hostname << ENDSSH
+cd services/${service}
+npm install
+pm2 restart ${service}
+ENDSSH
+
+printf "\n----> Removing local copy of the distribution package\n"
+rm -rf dist
